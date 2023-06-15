67db385e763c8cad34b44427cdf626630d4e683d
Valed7x <barnoucis@gmail.com>
2023-06-15 14:15:42 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase "programming is like building a multilingual puzzle
+ * Return: 0 if exited ptoperly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("/"programming is like bulding a multilingual puzzle");
+	return (0);
+}
