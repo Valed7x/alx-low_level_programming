9713c542177eac2d254aefb5c684a52c77f50050
Valed7x <barnoucis@gmail.com>
2023-06-16 14:07:02 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - enety point
+ *
+ * Description:Numberz
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
