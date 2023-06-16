9020301bbd33ae8ccd3f8b19a391736fdd45c6b9
Valed7x <barnoucis@gmail.com>
2023-06-16 14:12:02 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - enety point
+ *
+ * Description:Smile in the mirror
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
