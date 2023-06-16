9313283879d165a5360445bcdda86e199b987ba6
Valed7x <barnoucis@gmail.com>
2023-06-16 14:19:15 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - enety point
+ *
+ * Description:Hexadecimal
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
