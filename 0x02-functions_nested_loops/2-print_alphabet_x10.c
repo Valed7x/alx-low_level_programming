1d1a8c39677ccbb87ab405451f3d3e0a777faf89
Valed7x <barnoucis@gmail.com>
2023-06-20 10:28:46 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet, in lowercase,
+ * by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char h;
+	int e;
+
+	e = 0;
+
+	while (e < 10)
+	{
+		h = 'a';
+		while (h <= 'z')
+		{
+			_putchar(h);
+			h++;
+		}
+		_putchar('\n);
+		e++;
+	}
+}
