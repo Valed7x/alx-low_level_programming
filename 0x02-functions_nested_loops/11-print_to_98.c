3877d9ff95334d9cc91e8598e516c0902a499667
Valed7x <barnoucis@gmail.com>
2023-06-20 16:35:54 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,12 +19,11 @@ void print_to_98(int z)
 			else if (a == 98)
 				printf("%d\n", a);
 		}
-	}
-	else if (z >= 98)
+	} else if (z >= 98)
 	{
 		for (b = z; b >= 98; b--)
 		{
-			if (b == 98)
+			if (b != 98)
 				printf("%d, ", b);
 			else if (b == 98)
 				printf("%d\n", b);
