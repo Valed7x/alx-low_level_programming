3f243c83b8487319d2b57c6c132894cceaadf92e
Valed7x <barnoucis@gmail.com>
2023-06-26 15:15:40 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-
 /**
- * _puts - prints a string to stdout
+ * _puts - prints a string, followed by a new line, to stdout
  *
- * @str: pointer to the string to print
+ * @str: string to print
 */
 
 void _puts(char *str)
-
 {
 	while (*str != '\0')
 	{
