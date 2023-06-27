6368a91cbcd56df11746562a70340da97ba6cfa8
Valed7x <barnoucis@gmail.com>
2023-06-26 18:37:25 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - program that generates random valid.
+ *
+ * passwords for the program 101-crackme.
+ *
+ * Return: 0 (Success)
+*/
+
+
+int main(void)
+
+{
+	int num;
+	char a;
+
+	srand(time(NULL));
+	while (num <= 2513)
+	{
+		a = rand() % 123;
+		num += a;
+		putchar(a);
+	}
+	putchar(2873 - num);
+	return (0);
+}
