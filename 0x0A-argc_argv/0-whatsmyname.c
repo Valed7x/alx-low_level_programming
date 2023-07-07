405176cad7f7f04d466cb2a880c61dca8105537b
Valed7x <barnoucis@gmail.com>
2023-07-07 14:44:37 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - main function
+ * @argc: argument count
+ * @argv: pointer to string, and also argument vector
  *
- * Return: Always 0 (Success)
+ * Return: Alway return 0.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	int index1 = 0, index2;
 
+	while (index1 < argc)
+	{
+		for (index2 = 0; argv[index1][index2]; index2++)
+		{
+			_putchar(argv[index1][index2]);
+		}
+		index1++;
+	}
+	_putchar('\n');
 	return (0);
 }
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,24 +1,12 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
- * main - main function
- * @argc: argument count
- * @argv: pointer to string, and also argument vector
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Alway return 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriattely.
  */
-int main(int argc, char *argv[])
+int _putchar(char c)
 {
-	int index1 = 0, index2;
-
-	while (index1 < argc)
-	{
-		for (index2 = 0; argv[index1][index2]; index2++)
-		{
-			_putchar(argv[index1][index2]);
-		}
-		index1++;
-	}
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
