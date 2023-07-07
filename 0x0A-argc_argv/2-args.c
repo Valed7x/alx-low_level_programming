6be96125966a7d6bf079d2475cdce4cd43079307
Valed7x <barnoucis@gmail.com>
2023-07-07 15:06:48 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints all arguments it receives.
+ *
+ * @argc: number of command line.
+ *
+ * @argv: array that contains the program command line.
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+
+
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
