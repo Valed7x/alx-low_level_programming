b0162dcfc096470cef3df0d4a49a996d63285c7f
Valed7x <barnoucis@gmail.com>
2023-07-11 16:36:57 -0700

    It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -20,7 +20,7 @@ void free_grid(int **grid, int height)
 	if (grid == NULL || height == 0)
 		return;
 
-	for (i = 0; a < height; a++)
+	for (a = 0; a < height; a++)
 		free(grid[a]);
 
 	free(grid);
