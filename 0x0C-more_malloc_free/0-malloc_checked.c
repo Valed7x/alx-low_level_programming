d11ed36a4b8d3802fa3410062c93c250c30946e0
Valed7x <barnoucis@gmail.com>
2023-07-13 15:34:31 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,14 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
+#include "main.h"
 
 /**
- * *create_array - creates an array of chars.
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
  *
- * @size: size of the array.
- *
- * @c: char to initialize the array c.
- *
- * Return: NULL 0
-*/
-
+ * Return: pointer to the array (Success), NULL (Error)
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *p;
