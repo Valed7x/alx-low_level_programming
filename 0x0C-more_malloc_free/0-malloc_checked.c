04214da7a03cc36702511f842d555d5538f7156a
Valed7x <barnoucis@gmail.com>
2023-07-13 15:28:50 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+/**
+ * *create_array - creates an array of chars.
+ *
+ * @size: size of the array.
+ *
+ * @c: char to initialize the array c.
+ *
+ * Return: NULL 0
+*/
+
+
+char *create_array(unsigned int size, char c)
+
+{
+	char *eb;
+	unsigned int z;
+
+	if (size == 0)
+		return (NULL);
+
+	eb = malloc(sizeof(c) * size);
+
+	if (eb == NULL)
+		return (NULL);
+
+	for (z = 0; z < size; z++)
+		eb[z] = c;
+
+	return (eb);
+}
