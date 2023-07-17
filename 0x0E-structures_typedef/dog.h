1f2b0cfd61dcd15e1368355dcf415859162be23a
Valed7x <barnoucis@gmail.com>
2023-07-17 12:00:51 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,37 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+
+/**
+ * struct dog - data type for dogs.
+ *
+ * @name: its name
+ *
+ * @age: its age
+ *
+ * @owner: ins owner
+*/
+
+
+struct dog
+
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+
+#endif
