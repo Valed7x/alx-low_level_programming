aa86e366f2b5e1964520df4eb0ceed530ac28abd
Valed7x <barnoucis@gmail.com>
2023-08-01 17:42:58 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,24 +25,37 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	return (-1);
 	if (*head == NULL)
 	return (-1);
+
 	/*init*/
+
 	lok = *head;
 	lis = NULL;
 	while (lok != NULL && j < index)
+
 	{
+
 	lis = lok;
 	lok = lok->next;
 	j++;
+
 	}
+
 	if (lok == NULL)
 	return (-1);
 	if (lis == NULL)
+
 	{
+
 	*head = lok->next;
+
 	}
+
 	else
+
 	{
+
 	lis->next = lok->next;
+
 	}
 	free(lok);
 
