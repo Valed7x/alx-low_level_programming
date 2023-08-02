ea1981d2165f35bc3def723e58e7ab0b42062255
Valed7x <barnoucis@gmail.com>
2023-08-01 18:28:51 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,29 +15,29 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *lok = NULL;
 	const listint_t *lis = NULL;
-	size_t counter = 0;
-	size_t new_n;
+	size_t q = 0;
+	size_t new;
 
 	lok = head;
 	while (lok)
 	{
 		printf("[%p] %d\n", (void *)lok, lok->n);
-		counter++;
+		node++;
 		lok = lok->next;
 		lis = head;
-		new_n = 0;
-		while (new_n < counter)
+		new = 0;
+		while (new < node)
 		{
 			if (lok == lis)
 			{
 				printf("-> [%p] %d\n", (void *)lok, lok->n);
-				return (counter);
+				return (node);
 			}
 			lis = lis->next;
-			new_n++;
+			new++;
 		}
 		if (!head)
 			exit(98);
 	}
-	return (counter);
+	return (node);
 }
