75670a18bb3f63863d362188e7a8526f8f9b8573
Valed7x <barnoucis@gmail.com>
2023-08-03 16:32:54 -0700

    0-0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, i;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		num <<= 1;
-		num += b[i] - '0';
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (num);
+
+	return (ui);
 }
