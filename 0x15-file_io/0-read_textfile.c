d41fe566fcaade741edec36ee365609eaf1352ac
Valed7x <barnoucis@gmail.com>
2023-08-07 16:49:47 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,22 +22,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *ou;
 
 	if (!filename)
-
 		return (0);
 
 	lok = open(filename, O_RDONLY);
 
 	if (lok == -1)
-
 		return (0);
 
 	ou = malloc(sizeof(char) * (letters));
-
 	if (!ou)
 		return (0);
 
 	nod = read(lok, ou, letters);
-
 	lis = write(STDOUT_FILENO, ou, nod);
 
 	close(lok);
