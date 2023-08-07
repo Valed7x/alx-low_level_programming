ab893a99f09456b6fe12077c94c884e3615c22b4
Valed7x <barnoucis@gmail.com>
2023-08-07 16:44:32 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+/**
+ * read_textfile - reads a text file and prints letter.
+ *
+ * @filename: file.
+ *
+ * @letters: letters.
+ *
+ * Return: returns 0.
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+	int lok;
+
+	ssize_t nod, lis;
+
+	char *ou;
+
+	if (!filename)
+
+		return (0);
+
+	lok = open(filename, O_RDONLY);
+
+	if (lok == -1)
+
+		return (0);
+
+	ou = malloc(sizeof(char) * (letters));
+
+	if (!ou)
+		return (0);
+
+	nod = read(lok, ou, letters);
+
+	lis = write(STDOUT_FILENO, ou, nod);
+
+	close(lok);
+
+	free(ou);
+
+	return (lis);
+}
