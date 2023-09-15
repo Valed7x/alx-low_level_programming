aa232c94b7ff94d7ab153517bd3e665f46367254
Valed7x <barnoucis@gmail.com>
2023-09-14 17:51:55 -0700

    f

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,15 +2,12 @@
 
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given positions
- *
- * @h: double pointer to the beginning of the linked list.
- * 
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the beginning of the linked list
  * @idx: index at which to insert the new node
- *
  * @n: data to enter into new node
  *
- * Return: pointer to the new node.
+ * Return: pointer to the new node, or NULL on failure
 */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
